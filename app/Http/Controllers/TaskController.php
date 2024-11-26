8ffde89625a8ec51d654922a5a7683553cb27983
infinitypaul <infinitypaul@live.com>
2024-11-26 17:51:07 +0100

    Add task management with validation rules
    Introduced `TaskController` to handle basic CRUD operations for tasks including index, store, update, show, and destroy actions. Also added `StoreTaskRequest` and `UpdateTaskRequest` to manage validation logic for creating and updating tasks, improving request validation and authorization.

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $user = auth()->user();
+
+        $tasks = $user->tasks()->orderBy('created_at')->get();
+
+        return response()->json([
+            'tasks' => $tasks
+        ]);
+    }
+
+    public function store(StoreTaskRequest $request)
+    {
+        $user = auth()->user();
+
+        $task = $user->tasks()->create($request->validated());
+
+        return response()->json([
+            'message' => 'Task created!',
+            'task' => $task
+        ]);
+    }
+
+
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $task->name = $request->name;
+        $task->save();
+
+        return response()->json([
+            'message' => 'Task created!',
+            'task' => $task
+        ]);
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return response()->json([
+            'message' => 'Task deleted!'
+        ]);
+    }
+
+    public function show(Task $task)
+    {
+        return response()->json([
+            'task' => $task
+        ]);
+    }
+
+
+}
